fcae709c55de5d55c219183755eac388d42d53dd
Joseph Peterson <jscpeterson@gmail.com>
2018-10-30 16:24:46 -0600

    Set up rules converter and wrote some pseudocode for how to approach

--- app/src/main/java/edu/cnm/deepdive/videopoker/model/entity/PokerHand.java
+++ app/src/main/java/edu/cnm/deepdive/videopoker/model/entity/PokerHand.java
@@ -3,41 +3,18 @@ package edu.cnm.deepdive.videopoker.model.entity;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.support.annotation.NonNull;
+import edu.cnm.deepdive.videopoker.model.Card;
 
 @Entity
-public enum PokerHand {
-  ROYAL_FLUSH("Royal Flush", 250, 4000),
-  STRAIGHT_FLUSH("Straight Flush", 50),
-  FOUR_ACES("Four Aces"),
-  FOUR_OF_A_KIND("Four of a Kind", 25),
-  FULL_HOUSE("Full House", 9),
-  FLUSH("Flush", 5),
-  STRAIGHT("Straight", 4),
-  THREE_OF_A_KIND("Three of a Kind", 3),
-  TWO_PAIR("Two Pair", 2),
-  JACKS_OR_BETTER("Jacks or Better", 1),
-  BUST("\u2639");
+public class PokerHand {
 
   public static final int DEFAULT_VALUE = 0;
   public static final int MAX_BET = 5;
 
-  PokerHand(String name, int betOneValue, int betFiveValue) {
-    this.name = name;
-    this.betOneValue = betOneValue;
-    this.betFiveValue = betFiveValue;
-  }
-
-  PokerHand(String name, int betOneValue) {
-    this.name = name;
-    this.betOneValue = betOneValue;
-    this.betFiveValue = betOneValue * MAX_BET;
-  }
-
-  PokerHand(String name) {
-    this.name = name;
-    this.betOneValue = DEFAULT_VALUE;
-    this.betFiveValue = DEFAULT_VALUE;
-  }
+  @NonNull
+  @ColumnInfo
+  //the big toledo
+  private String rules;
 
   @NonNull
   @ColumnInfo
@@ -50,5 +27,37 @@ public enum PokerHand {
   @NonNull
   @ColumnInfo
   private int betFiveValue;
+
+  public class PokerHandRuleConverter {
+    /*
+    Card:: = suit, rank
+    Suit::= wildcard | suit literal | carry
+    Suit literal ::= C|D|H|S
+    Wildcard ::= *
+    Rank ::= wildcard | offset | [op]rank literal
+    Offset ::= -n | 0 | n
+    op::= = | >= | <= | > | <
+    Rank.literal::= 2|3|4|5|6|7|8|9|T|J|Q|K|A
+    */
+
+    //Cards sort first by rank then suit
+
+    //Rules string format
+    //For cards a two char string represents a char - A♠ is Ace of Spades
+    //For the rules set each two char string represents a matching rule
+    // - a card would be represented by two matching rules for a suit matching rule and a rank matching rule
+
+    //char 0 being the first card, char 1 being the following card to compare against char 0
+
+    //A flush for example might be
+    //**,=*;**,=*;**,=*;**,=*;**,=*;
+    //RANK (first char set) is irrelevant, all wildcard matches
+    //SUIT requires a rule on each so that the first item matches the next item
+
+    boolean checkHandAgainstRules(String rules, Card[] hand) {
+       return false;
+    }
+
+  }
 
 }
