e52c3dcb55dd83bef3319b53bd0c146257d7027b
jscpeterson <31524025+jscpeterson@users.noreply.github.com>
2018-11-25 17:40:29 -0700

    Minor changes to DDL

--- docs/DDL.sql
+++ docs/DDL.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `Paytable` (
     `name`        TEXT COLLATE NOCASE
 );
 
-CREATE TABLE IF NOT EXISTS `PokerHand`
+CREATE TABLE IF NOT EXISTS `PokerHand` (
     `poker_hand_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
     `paytable_id`   INTEGER NOT NULL
     `name`          TEXT
@@ -12,6 +12,6 @@ CREATE TABLE IF NOT EXISTS `PokerHand`
     `betFiveValue`  INTEGER NOT NULL
     `showInTable`   INTEGER NOT NULL
     FOREIGN KEY(`paytable_id`) REFERENCES `Paytable`(`paytable_id`) ON UPDATE NO ACTION ON DELETE NO ACTION 
-    );
+);
         
-CREATE INDEX `index_PokerHand_paytable_id` ON `${TABLE_NAME}` (`paytable_id`)"
+CREATE INDEX `index_PokerHand_paytable_id` ON `PokerHand` (`paytable_id`)
